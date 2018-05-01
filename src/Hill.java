4c488b4c9c9f374ac6b1b7c59e8d71bc9e41ae28
cxp5606 <cxp5606@rit.edu>
2018-04-30 23:45:19 -0400

    User entry fixed
    double asked for user entered key.
    fixed it.

--- src/Hill.java
+++ src/Hill.java
@@ -14,72 +14,68 @@ public class Hill {
 
         //Set up input read
         DataInputStream dis = new DataInputStream(System.in);
-
-        //Input message
-        String message = "";
-        System.out.println("Please input your message.");
-        try {
-            message=dis.readLine();
-        }
-        catch (Exception e){}
-
-        //Create matrix
-        matrix.Matrix(message);
-
-        //Enter key?
-        String userkey="";
+        String again="";
         do {
-            System.out.println("Do you want to enter the encrypting key?(y/n)");
-            try {
-                userkey=dis.readLine();
-                userkey=userkey.toLowerCase();
-            }
-            catch (Exception e){}
-            if (userkey.equals("y")) {
-                String uk = "";
-                System.out.println("Please enter the encrypting key string");
-                try {
-                    uk = dis.readLine();
-                } catch (Exception e) {
-                }
-                key.fillK(uk);
-            }
-        }while(!userkey.equals("y") && !userkey.equals("n"));
-        if(userkey.equals("y"))
-        {
-            String keying = "";
-            System.out.println("Please input the key.");
-            try {
-                keying=dis.readLine();
-            }
-            catch (Exception e){}
-            key.MKey(message);
-            key.fillK(keying);
-        }
-        else
-        {
 
-            key.MKey(message);
-            key.fillK();
-        }
-        //get process
-        String process = "";
-        do {
-            System.out.println("Encrypt(e) or Decrypt(d).");
+            //Input message
+            String message = "";
+            System.out.println("Please input your message.");
             try {
-                process = dis.readLine();
-                process = process.toLowerCase();
+                message = dis.readLine();
             } catch (Exception e) {
             }
-        }while(!process.equals("e") && !process.equals("d"));
-        if (process.equals("d") && userkey.equals("n"))
-        {
-            key.setK(key.getI());
-        }
-        encrypt.Crypt(matrix,key);
-        encrypt.Mult();
 
-        System.out.println("The resulting matrix is:\n" + encrypt.getCrypt().toString() + "\nThe message it holds is:\n"
+            //Create matrix
+            matrix.Matrix(message);
+
+            //Enter key?
+            String userkey = "";
+            do {
+                System.out.println("Do you want to enter the encrypting key?(y/n)");
+                try {
+                    userkey = dis.readLine();
+                    userkey = userkey.toLowerCase();
+                } catch (Exception e) {
+                }
+            } while (!userkey.equals("y") && !userkey.equals("n"));
+            String uk = "";
+            key.MKey(message);
+            if (userkey.equals("y")) {
+                do {
+                    System.out.println("Please enter the encrypting key string");
+                    try {
+                        uk = dis.readLine();
+                    } catch (Exception e) {
+                    }
+                }
+                while (uk.trim().replaceAll("[^A-Za-z]+", "").toUpperCase().length() != (key.getMatrix().getRows() * key.getMatrix().getCols()));
+                key.fillK(uk);
+            } else {
+                key.fillK();
+            }
+            //get process
+            String process = "";
+            do {
+                System.out.println("Encrypt(e) or Decrypt(d).");
+                try {
+                    process = dis.readLine();
+                    process = process.toLowerCase();
+                } catch (Exception e) {
+                }
+            } while (!process.equals("e") && !process.equals("d"));
+            if (process.equals("d") && userkey.equals("n")) {
+                key.setK(key.getI());
+            }
+            encrypt.Crypt(matrix, key);
+            encrypt.Mult();
+
+            System.out.println("The resulting matrix is:\n" + encrypt.getCrypt().toString() + "\nThe message it holds is:\n"
                     + encrypt.getCrypt().message());
+            System.out.println("Do you want to go again? (y/n)");
+            try {
+                again = dis.readLine();
+            } catch (Exception e) {
+            }
+        }while(again.equals("y") || again.equals("Y"));
     }
 }
